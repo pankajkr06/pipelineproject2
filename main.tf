df6d6a65a4951c9abed5c01f6e1f627585daf437
pankajkr06 <pankajkr06@gmail.com>
2022-05-29 18:58:40 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+    type = string
+    default = "eu-west-1"
+  
+}
+
+module "myvpc" {
+    source = "./Module"
+    az = ["eu-west-1a"]
+    pub_sub = ["10.10.10.0/24"]
+    vpc_name = "Lab"
+    cidr = "10.10.0.0/16"
+      
+}
